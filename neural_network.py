7158256fd15a96768e4428c33a04b0c749007e2b
kanjurer <kanavb9999@gmail.com>
2024-12-22 22:17:32 -0400

    Added

---
+++ neural_network.py
@@ -0,0 +1,27 @@
+from torch import nn
+import torch
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+input_image = torch.rand(3, 28, 28)
+print(input_image.size())
+flatten = nn.Flatten()
+flat_img = flatten(input_image)
+print(flat_img.size())
---
+++ tokenizer.py
@@ -0,0 +1,7 @@
+import re
+
+
+def tokenize(text):
+    pattern = re.compile(r'[A-Za-z]+[\w^\']*|[\w^\']*[A-Za-z]+[\w^\']*')
+    return pattern.findall(text.lower())
+
